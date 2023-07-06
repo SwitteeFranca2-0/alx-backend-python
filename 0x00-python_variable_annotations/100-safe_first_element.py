f4cf8aeadc027ea6300acbd37f865380c5335310
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-06 17:03:05 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Union, Sequence, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """returns teh first element of an iterable"""
     if lst:
         return lst[0]
     else:
