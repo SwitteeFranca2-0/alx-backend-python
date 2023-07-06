2479d24de74cb60cb7da2a7476d41707187221ca
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-06 17:01:22 +0100

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Safe first element"""
-from typing import Union, Sequence, Any, NoneType
+from typing import Union, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Safe first element"""
-from typing import Mapping, Any, Union, Any, NoneType, TypeVar
+from typing import Mapping, Any, Union, Any, TypeVar
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, NoneType] = None) -> Union[Any, T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """sfaely get value"""
     if key in dct:
         return dct[key]
