0c4e0734c68ebe6050a59a5efd423864a4e3fa21
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-10 23:48:22 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Taks on asyncio python"""
 
-from typing import List
 import asyncio
+import typing
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> typing.List:
     """Run a concurrent function with it"""
     tsks = [task_wait_random(max_delay) for i in range(n)]
     return [await task for task in asyncio.as_completed(tsks)]
