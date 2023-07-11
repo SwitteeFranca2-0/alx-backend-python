34b91ab912f6246c2f69787828c7fd44dfa00d5d
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-11 14:15:11 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Running concurrent functions"""
 
-from typing import List
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run a concurrent function with it"""
-    tasks = [asyncio.create_task(wait_random(max_delay) for i in range(n))]
-    return [await task for task in asyncio.as_completed(tasks)]
+    vals = [wait_random(max_delay) for i in range(0, n)]
+    val = await asyncio.gather(*vals)
+    return sorted(val)
